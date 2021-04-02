7efb652f7e9ca5d0ca4dd9eac11d46bae8dda721
Tiago Vilela <tiagovla@gmail.com>
2021-04-02 16:53:58 -0300

    Rearranging Project.

--- fdtd/__init__.py
+++ fdtd/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 
 import logging
 
@@ -8,11 +8,7 @@ from fdtd.grid import Grid
 from fdtd.lumped_elements import Capacitor, Diode, Inductor, Resistor
 from fdtd.materials import Material
 from fdtd.objects import Brick, Sphere
-from fdtd.sources import (
-    EFieldSource,
-    ImpressedElectricCurrentSource,
-    ImpressedMagneticCurrentSource,
-    VoltageSource,
-)
+from fdtd.sources import (EFieldSource, ImpressedElectricCurrentSource,
+                          ImpressedMagneticCurrentSource, VoltageSource)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
--- tests/test_fdtd.py
+++ tests/test_fdtd.py
@@ -3,4 +3,4 @@ from fdtd import __version__
 
 def test_version():
     """Test version of the package."""
-    assert __version__ == "0.0.1"
+    assert __version__ == "0.0.3"
