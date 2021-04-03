011e1f6f3b4addd416f7622eeba8a1ad7fe96126
Tiago Vilela <tiagovla@gmail.com>
2021-04-02 21:15:24 -0300

    Changed private attributes to public.

--- fdtd/lumped_elements.py
+++ fdtd/lumped_elements.py
@@ -128,14 +128,14 @@ class Resistor(LumpedElement):
     def attach_to_grid(self):
         """Attach object to grid."""
         self.idx_s = (
-            np.argmin(np.abs(self.grid._x - self.x_min)),
-            np.argmin(np.abs(self.grid._y - self.y_min)),
-            np.argmin(np.abs(self.grid._z - self.z_min)),
+            np.argmin(np.abs(self.grid.x - self.x_min)),
+            np.argmin(np.abs(self.grid.y - self.y_min)),
+            np.argmin(np.abs(self.grid.z - self.z_min)),
         )
         self.idx_e = (
-            np.argmin(np.abs(self.grid._x - self.x_max)),
-            np.argmin(np.abs(self.grid._y - self.y_max)),
-            np.argmin(np.abs(self.grid._z - self.z_max)),
+            np.argmin(np.abs(self.grid.x - self.x_max)),
+            np.argmin(np.abs(self.grid.y - self.y_max)),
+            np.argmin(np.abs(self.grid.z - self.z_max)),
         )
 
         dx, dy, dz = self.grid.spacing
